edfb32c30f67ae3667ca23b25a7ebe6e658112d7
cleriphil <cleri.phil@gmail.com>
2015-10-09 21:42:53 -0700

    edit auction to include the user email

--- app/addAuction/addAuction.js
+++ app/addAuction/addAuction.js
@@ -10,22 +10,23 @@ angular.module('myApp.addAuction', ['ui.router', 'firebase'])
     });
 })
 
-.controller('AddAuctionCtrl', ['$scope', '$firebaseObject', function($scope, $firebaseObject) {
+.controller('AddAuctionCtrl', ['$scope', '$firebaseObject', 'CommonProp', function($scope, $firebaseObject, CommonProp) {
   $scope.addAuction = function() {
-    var ref = new Firebase("https://art-app.firebaseio.com");
-    var auctionsRef = ref.child("auctions");
 
     var title = $scope.auction.title;
     var description = $scope.auction.description;
     var startDate = $scope.auction.startDate.toString();
     var endDate = $scope.auction.endDate.toString();
 
+    var ref = new Firebase("https://art-app.firebaseio.com");
+    var auctionsRef = ref.child("auctions");
 
     auctionsRef.push().set({
       title: title,
       description: description,
       startDate: startDate,
-      endDate: endDate
+      endDate: endDate,
+      emailId: CommonProp.getUser()
     },
     function(error) {
       if (error) {
